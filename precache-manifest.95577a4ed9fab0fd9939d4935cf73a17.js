507778e80ccbda588475195dfe87f3d98819a342
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-10-16 08:47:44 -0500

    Updates

---
+++ precache-manifest.95577a4ed9fab0fd9939d4935cf73a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38a3ab7195e3187d726d078cb4bd9080",
+    "url": "/static/media/logo.38a3ab71.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da025aacc6c726daf42d",
+    "url": "/static/js/main.da025aac.chunk.js"
+  },
+  {
+    "revision": "56d00a64e5c716f0044b",
+    "url": "/static/js/1.56d00a64.chunk.js"
+  },
+  {
+    "revision": "da025aacc6c726daf42d",
+    "url": "/static/css/main.dc9c4fba.chunk.css"
+  },
+  {
+    "revision": "49fea1ceda5a7f69af1152a69117d538",
+    "url": "/index.html"
+  }
+];
